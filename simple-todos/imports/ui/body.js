0540aa5d45c635e96cef4b0c0c351728d096d1d8
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-24 09:42:46 +0530

    added collections(dB)

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,11 +1,10 @@
 import { Template } from 'meteor/templating';
+import { Task } from '../api/tasks';
  
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks() {
+    return Task.find({});
+  }
 });
