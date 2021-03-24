f06d604c80483abeff675b66f09dabca2689bba2
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-24 13:52:46 +0530

    updated user Login

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,14 +1,29 @@
+import {Meteor} from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import {ReactiveDict} from 'meteor/reactive-dict';
+
 import { Task } from '../api/tasks';
 
 import "./task";
 import "./task.html";
  
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated () {
+  this.state = new ReactiveDict();
+});
  
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      return Task.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      
+    }
     return Task.find({}, {sort: {createdAt: -1}});
+  },
+  incompletedCount(){
+    return Task.find({checked: {$ne: true}}).count(); 
   }
 });
 
@@ -24,8 +39,14 @@ Template.body.events({
     Task.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
 
+    // clear form !!
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
