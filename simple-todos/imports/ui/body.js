c57a01be9090327d9452f27be9302aab472a3a8d
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-24 10:17:27 +0530

    added forms and events

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -5,6 +5,24 @@ import './body.html';
  
 Template.body.helpers({
   tasks() {
-    return Task.find({});
+    return Task.find({}, {sort: {createdAt: -1}});
   }
+});
+
+
+Template.body.events({
+  
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Task.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    target.text.value = '';
+  },
 });
